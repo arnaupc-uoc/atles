0f5cf8260a70922b5e14b90b30abb15b2d9110b0
Arnau Pujol Cabarrocas <arnaupc@hotmail.com>
2026-06-07 21:35:36 +0200

    butons tooltip issue not resolved

--- backend/app/static/js/components/AdminTableActions.js
+++ backend/app/static/js/components/AdminTableActions.js
@@ -3,12 +3,26 @@ export default {
     actions: { type: Array, required: true }
   },
   emits: ['action-click'],
+  methods: {
+    onActionClick(event, action) {
+      // Forcem el desforç (blur) del botó actiu perquè perdi l'estat "marcat" immediatament
+      if (event && event.currentTarget) {
+        event.currentTarget.blur();
+      }
+
+      // Emetem l'esdeveniment cap al pare com fins ara
+      this.$emit('action-click', action);
+    }
+  },
   template: `
     <div class="d-flex ga-2 justify-center">
       <v-tooltip
         v-for="action in actions"
         :key="action.name"
         location="top"
+        :open-on-click="false"
+        :open-on-focus="false"
+        eager
       >
         <template #activator="{ props }">
           <v-btn
@@ -16,6 +30,7 @@ export default {
             icon
             variant="tonal"
             size="small"
+            density="comfortable"
             @click="$emit('action-click', action)"
           >
             <v-icon size="small">{{ action.icon }}</v-icon>
